d687716978e8fd01823fa8f76452fd305a85f4ad
TipsyTurtlz <buschke.tm@gmail.com>
2022-12-04 13:52:36 +0100

    first part of day 4

---
+++ Day 4/AoC_04_Cleanup.py
@@ -0,0 +1,32 @@
+########### PART ONE #################
+counter = 0
+
+# import
+input_data = open('C:/Users/busch/OneDrive/Desktop/Advent-of-Code-2022/Day 4/AoC_04_input_data.txt','r')
+# input_data = open('C:/Users/busch/OneDrive/Desktop/Advent-of-Code-2022/Day 4/test.txt','r')
+data = input_data.read()
+data_list = data.splitlines()   
+
+for string in data_list:
+    string_index = data_list.index(string)
+    data_list[string_index] = string.replace(',','-')
+    data_list[string_index] = data_list[string_index].split('-')
+
+# pair one inside pair two a>c && b<d
+# pair two inside pair one a<c && b>d
+for string in data_list:
+    
+    a = int(string[0])
+    b = int(string[1])
+    c = int(string[2])
+    d = int(string[3])
+    
+    if (a >= c and b <= d):
+        counter += 1
+        # print(f"{a}-{b} is in {c}-{d}")
+    elif a <= c and b >= d:
+        counter += 1
+        # print(f"{c}-{d} is in {a}-{b}")
+   
+    
+print(counter)
