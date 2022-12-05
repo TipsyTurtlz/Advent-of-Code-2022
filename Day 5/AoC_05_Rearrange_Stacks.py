a9a498d442ecb130d9a042fd499b36c6cb9b3e6f
TipsyTurtlz <buschke.tm@gmail.com>
2022-12-05 20:35:31 +0100

    reworked Day 5

--- Day 5/AoC_05_Rearrange_Stacks.py
+++ Day 5/AoC_05_Rearrange_Stacks.py
@@ -1,37 +1,29 @@
 ########### PART ONE #################
-stack_commands, command_list, stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9 = [], [], [], [], [], [],[],[],[],[],[]
+empty = '\n'
+# contains the indicies of where the information in each stack is saved
+position = []
+stack_content = []
+# line number where the stack numbers are located
+stack_numbers_line = 3
+stack_number_list = [1,2,3]
+
+with open('Day 5/test.txt') as file:
+    for line in enumerate(file):
+        stack_content.append(line[1].replace())
+        if line[0] == stack_numbers_line:
+            for number in stack_number_list:
+                position.append(line[1].index(str(number)))
+print(stack_content)
+           
 
 
-with open('Day 5/AoC_05_input_data.txt') as file:
-    for line in file:
-        
-        if line != '\n' and not 'move' in line:
-            line = line.replace('\n', '')
-            # more generalization!
-            stack1.append(line[1])  
-            stack2.append(line[5])  
-            stack3.append(line[9])  
-            stack4.append(line[13]) 
-            stack5.append(line[17]) 
-            stack6.append(line[21]) 
-            stack7.append(line[25]) 
-            stack8.append(line[29]) 
-            stack9.append(line[33]) 
 
-        elif 'move' in line:
-            line = line.replace('move', '')
-            line = line.replace('from', '')
-            line = line.replace('to', '')
-            line = line.replace('\n', '')
-            line = line.replace(' ','')
-            stack_commands.append(line)
-print([stack1,stack2, stack3])
+
+
+
+
+
+
 
        
-
-# for command in stack_commands:
-#     how_many = int(command[0])
-#     starting_stack = int(command[1])
-#     destination_stack = int(command[2])
-#     # print([how_many,starting_stack,destination_stack])
 
