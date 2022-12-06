4041348cacce8b288e7309c7f294788b773f62f8
TipsyTurtlz <buschke.tm@gmail.com>
2022-12-06 17:38:08 +0100

    finished Day 5

--- Day 5/AoC_05_Rearrange_Stacks.py
+++ Day 5/AoC_05_Rearrange_Stacks.py
@@ -1,142 +1,81 @@
 ########### PART ONE #################
-import numpy as np
-empty = '\n'
-# contains the indicies of where the information in each stack is saved
-stack_content = []
-# line number where the stack numbers are located
-stack_numbers = []
-move_commands = []
-indices = []
-
-stacks =[]
-
+empty  = '\n'
+stack_numbers, move_commands, solution = [], [], []
+crate_content, stack = {}, {}
+# with open('Day 5/test.txt') as file:
 with open('Day 5/AoC_05_input_data.txt') as file:
     for line in file:
-        # separate stack content from input
-        if line != empty and '1' not in line and 'move' not in line:
-            line = line.replace('\n','')
-            stack_content.append(line)
-        # separate stack numbers
-        if line != empty and 'move' not in line and '1' in line:
-            line = line.replace('\n','')
-            stack_numbers.append(line)
+        if 'move' not in line and line != empty:    
+            stacks = range(1,len(line),4)
+            for crate in stacks:
+                if line[crate].isdigit():
+                    stack_numbers.append(int(line[crate]))  
+                if line[crate].isupper():
+                    crate_content.setdefault(crate, []).append(line[crate])
         # separate move commands
         if line != empty and 'move' in line:
-            line = line.replace('\n','').replace('move','').replace('from','').replace('to','').replace(' ','')
-            move_commands.append(line)
+            line = line.replace('\n','').replace('move','').replace('from','-').replace('to','-').replace(' ','')
+            move_commands.append(line) # digits are strings in here!
 
+# # check reading output
+# print(crate_content)
+# print(move_commands)
 
-rows, columns, height = 72, 9, 8
-stack_array = np.zeros((rows,columns),dtype=str)
-# print(stack_array)
-print(stack_content)
-i=0
-for line in stack_content:
-    j=0
-    for char in line:
-        # if line.index(char)%4 ==1 and char != ' ':
-        #     stack_array[5+i][(j-1)/4] = char
-        # print(char)
-        if j%4 == 1 and char != ' ':
-            # print(char)
-            a=int(rows-height+i)
-            b=int((j-1)/4)
-            # print (a, b)
-            stack_array[a][b] = char
-        j+=1
-    i+=1
+# rearrange crate dict
+crate_content_sorted = sorted(crate_content)
+crate_content = {key:crate_content[key] for key in crate_content_sorted}
+# print(crate_content)
 
+# change keys from line position to stack number                
+for line_position,stack_number in zip(crate_content,stack_numbers):
+    stack[stack_number] = crate_content[line_position]
+    
 
+# # check output
+# print(stack)
 
-print(stack_array) # ['    [D]    ', '[N] [C]    ', '[Z] [M] [P]']
-# [['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' '' '' '' '' '' '' '']
-#  ['' '' 'F' 'Q' '' '' 'Q' '' '']
-#  ['B' '' 'Q' 'V' 'D' '' 'S' '' '']
-#  ['S' 'P' 'T' 'R' 'M' '' 'D' '' '']
-#  ['J' 'V' 'W' 'M' 'F' '' 'J' '' 'J']
-#  ['Z' 'G' 'S' 'W' 'N' 'D' 'R' '' 'T']
-#  ['V' 'M' 'B' 'G' 'S' 'C' 'T' 'V' 'S']
-#  ['D' 'S' 'L' 'J' 'L' 'G' 'G' 'F' 'R']
-#  ['G' 'Z' 'C' 'H' 'C' 'R' 'H' 'P' 'D']]
+for command in move_commands:    
+    how_many = int(command.split('-')[0])
+    starting_stack = int(command.split('-')[1])
+    destination_stack = int(command.split('-')[2])
+    
+########### PART TWO #################
+    crates_to_move = stack[starting_stack][0:how_many]
+    for crates in range(len(crates_to_move)-1,-1,-1):
+        stack[destination_stack].insert(0,crates_to_move[crates])
+    del stack[starting_stack][:len(crates_to_move)]
 
+# # check output
+# print(stack)
 
-#Pickup-Funktion
-def move(num, column, target)
+# get the result crates
+for crates in stack:
+    solution.append(stack[crates][0])     
 
-#for schleife num mal ausführen 
-#oberstes Element in Spalte column finden
-
-#oberstes Element zwischenspeichern in temp und löschen
-
-#oberstes element in target finden
-#temp oberhalb einfügen
+print(''.join([*solution]))
+        
 
 
 
-# def function(args):
-#     pass
 
-#Place-Funktion
+
+
+
+
+
+
+
+
+
+
+
+
+## TIL:
+#  *: Repetetion Operator on List Items -> returns a list
+# zip() function for Parallel Iteration
+# join() method returns a string by joining all the elements of an iterable (list, string, tuple), separated by the given separator
+# map() function applies a given function to each item of an iterable (list, tuple etc.) and returns an iterator. In combination with str casting method
+#       it converts each element of a list to the string type and returns the resultant list of items
+
+
+
